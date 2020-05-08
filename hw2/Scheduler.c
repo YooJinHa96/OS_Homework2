fcd2aa109c91a1e32b000385f7a94cadadc30a42
YooJinHa96 <yjs19162@hanmail.net>
2020-05-08 12:11:37 +0900

    RunSchedluer

--- hw2/Scheduler.c
+++ hw2/Scheduler.c
@@ -2,11 +2,17 @@
 #include "Init.h"
 #include "Thread.h"
 
-int RunScheduler(void) {}
+int RunScheduler(void) {
+
+    // Readyqueue empty check
+    for (int i = 0; i < MAX_READYQUEUE_NUM; i++) {
+        if (pReadyQueueEnt[i].queueCount) {
+        }
+    }
+}
 
 void __ContextSwitch(int curpid, int newpid) {
 
-    kill(curpid,SIGSTOP);
-    kill(newpid,SIGCONT);
-
+    kill(curpid, SIGSTOP);
+    kill(newpid, SIGCONT);
 }
